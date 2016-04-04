93002fb679604e2a4c8d99e22a0f064d246ae55c
David Toth <dtoth7012@gmail.com>
2016-04-04 16:46:54 -0500

    Finished the layout for the 2 condition pages except for song and buttons to switch between the pages

--- app/src/main/java/cs371m/weatherwake/ConditionsEditorActivity.java
+++ app/src/main/java/cs371m/weatherwake/ConditionsEditorActivity.java
@@ -1,7 +1,56 @@
 package cs371m.weatherwake;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+
 /**
  * Created by KC on 3/27/2016.
  */
-public class ConditionsEditorActivity {
+public class ConditionsEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_wake_condition_editor);
+
+        // Setup the new range seek bar
+        RangeSeekBar<Integer> tempRSB = new RangeSeekBar<Integer>(this);
+        // Set the range
+        tempRSB.setRangeValues(-30, 120);
+        tempRSB.setSelectedMinValue(85);
+        tempRSB.setSelectedMaxValue(120);
+
+        // Add to layout
+        LinearLayout temp_layout = (LinearLayout) findViewById(R.id.tempRSB);
+        temp_layout.addView(tempRSB);
+
+        // Setup the new range seek bar
+        RangeSeekBar<Integer> precipitationRSB = new RangeSeekBar<Integer>(this);
+        // Set the range
+        precipitationRSB.setRangeValues(0, 100);
+        precipitationRSB.setSelectedMinValue(0);
+        precipitationRSB.setSelectedMaxValue(1);
+
+        // Add to layout
+        LinearLayout precipitation_layout = (LinearLayout) findViewById(R.id.precipitationRSB);
+        precipitation_layout.addView(tempRSB);
+
+        // Setup the new range seek bar
+        RangeSeekBar<Integer> windRSB = new RangeSeekBar<Integer>(this);
+        // Set the range
+        windRSB.setRangeValues(0, 50);
+        windRSB.setSelectedMinValue(0);
+        windRSB.setSelectedMaxValue(5);
+
+        // Add to layout
+        LinearLayout layout = (LinearLayout) findViewById(R.id.windRSB);
+        layout.addView(tempRSB);
+
+    }
+
 }
