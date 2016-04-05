74bb14607cdedd9b2e0faab520b171c1d119b8de
Kevin Chi <haoc92@gmail.com>
2016-04-04 21:55:52 -0500

    add weather setting path

--- app/src/main/java/cs371m/weatherwake/WeatherSettingsActivity.java
+++ app/src/main/java/cs371m/weatherwake/WeatherSettingsActivity.java
@@ -1,7 +1,40 @@
 package cs371m.weatherwake;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by KC on 3/27/2016.
  */
-public class WeatherSettingsActivity {
+public class WeatherSettingsActivity extends Activity {
+    private TextView mWeather;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_wake_conditions);
+
+        setTextViewInfo();
+
+        mWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prepConditionsEditorActivity();
+            }
+        });
+
+
+    }
+
+    private void setTextViewInfo() {
+        mWeather = (TextView) findViewById(R.id.pairing1);
+    }
+
+    private void prepConditionsEditorActivity(){
+        Intent intent = new Intent(this, ConditionsEditorActivity.class);
+        startActivity(intent);
+    }
 }
