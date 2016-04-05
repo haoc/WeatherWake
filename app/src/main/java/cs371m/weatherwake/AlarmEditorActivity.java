08ef3d2aabaef388feeb24afba5560a181ffd6b1
Kevin Chi <haoc92@gmail.com>
2016-04-04 19:55:24 -0500

    Update and rename ALarmEditorActivity.java to AlarmEditorActivity.java

--- app/src/main/java/cs371m/weatherwake/AlarmEditorActivity.java
+++ app/src/main/java/cs371m/weatherwake/AlarmEditorActivity.java
@@ -1,10 +1,12 @@
 package cs371m.weatherwake;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
 
 /**
  * Created by KC on 3/27/2016.
